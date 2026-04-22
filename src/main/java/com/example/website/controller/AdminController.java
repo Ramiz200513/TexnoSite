ca742e516d8b48c8001b94b6427119a8ccd6611c
Ramiz <feisonds@gmail.com>
2026-04-22 09:25:00 +0600

    new sovle

--- src/main/java/com/example/website/controller/AdminController.java
+++ src/main/java/com/example/website/controller/AdminController.java
@@ -89,14 +89,14 @@ public class AdminController {
             @RequestParam(defaultValue = "id_desc") String sort,
             jakarta.servlet.http.HttpServletResponse response) throws IOException {
 
-//        Sort sortObj = sort.equals("price_asc") ? Sort.by("price").ascending() :
-//                sort.equals("price_desc") ? Sort.by("price").descending() : Sort.by("id").descending();
+        Sort sortObj = sort.equals("price_asc") ? Sort.by("price").ascending() :
+                sort.equals("price_desc") ? Sort.by("price").descending() : Sort.by("id").descending();
 
-        Sort sortObj = switch (sort) {
-            case "price_asc" -> Sort.by("price").ascending();
-            case "price_desc" -> Sort.by("price").descending();
-            default -> Sort.by("id").descending();
-        };
+//        Sort sortObj = switch (sort) {
+//            case "price_asc" -> Sort.by("price").ascending();
+//            case "price_desc" -> Sort.by("price").descending();
+//            default -> Sort.by("id").descending();
+//        };
 
         List<Product> products = productRepository.findFiltered(
                 keyword, category, minPrice, maxPrice, PageRequest.of(0, 1000, sortObj)
